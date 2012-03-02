06861894f8190d9f93a3ba74221d769a7d261f0c
Bryan Kerrigan <bryankerrigan@gmail.com>
2012-03-02 10:40:23 -0800

    Fixed a couple bugs with the program: 1. wasn't converting to lower case when checking words removed from snippet 2. final count of snippet size was off by one
    Cleaned up the code and some comments

--- doc_search/doc_search.php
+++ doc_search/doc_search.php
@@ -3,6 +3,10 @@ Class DocSearch {
 	public $file;
 	public $words = array();
 
+	/*
+	 * Parse the arguments, verify the file exists and start the search
+	 * Input: an array of arguments of format - <file> <search word1> <search word 2> ...
+	 */
 	public function __construct($args) {
 		$arg_size = count($args);
 		for ($i = 1; $i < $arg_size; $i++) {
@@ -20,7 +24,7 @@ Class DocSearch {
 	}
 
 	/* 
-	 * Opens the given file, searhces for the specified key words
+	 * Opens the given file, searches for the specified key words
 	 */
 	public function do_search() {
 		$snippet = array();
@@ -42,32 +46,35 @@ Class DocSearch {
 		
 		// start processing the file
 		$file = @fopen($this->file, "r");
-		if ($file == false) {
+		if ($file === false) {
 			exit("Error: Unable to open file $this->file\n");
 		}
 
 		while (($buf = fgets($file)) !== false) {
 			$buf = explode(" ", $buf);
-			foreach ($buf as $w) {
-				$w_trimmed = strtolower(trim($w, ",.;:/\\ \n\t\r")); 
+			foreach ($buf as $word) {
+				$word_trimmed = strtolower(trim($word, ",.;:/\\ \n\t\r")); // strip off any punctuation, cast to lower case
 				if ($add_to_snippet) {
-					array_push($snippet, $w);	// don't add to the snippet until we've found the first search word
+					array_push($snippet, $word);	// don't add to the snippet until we've found the first search word
 				}
 
-				if (in_array($w_trimmed, $search_words)) {
-					$matches[$word_count] = $w_trimmed;	// add the word to the list of matching words with its position
+				if (in_array($word_trimmed, $search_words)) {
+					$matches[$word_count] = $word_trimmed;	// add the word to the list of matching words with its position
 
 					// if this is the first match, start recording the text snippet
 					if (!$add_to_snippet) {
 						$add_to_snippet = true;
-						array_push($snippet, $w);
+						array_push($snippet, $word);
 					}
 					
 					/* process the array of matching words
 					 * number of matches is >= the number of words searched for if:
 					 * Case 1: The first matching word is repeated in the list
+					 *	Action: remove the first match and continue
 					 * Case 2: All search words are found and we have a valid snippet
+					 *	Action: check the length versus the shortest so far, remove the first match and continue
 					 * Case 3: Not all search words are found, at least one aside from the first is repeated
+					 *	Action: continue without modifying the list of matches
 					 */
 					while (count($matches) >= count($search_words)) {
 						$first_word = null;
@@ -90,31 +97,36 @@ Class DocSearch {
 						}
 
 						if ($word_check[$first_word] > 1) {
-							// Case 1: the first word in the matches occurs twice
-							// shift the first match out of the array
+							/* Case 1: the first word in the matches occurs twice
+							 * remove the first match from the array
+							 */
 							unset($matches[$first_position]);
 						} else if (count($word_check) >= count($search_words)) {
-							// Case 2: we've found a valid snippet
-							// check its length versus the best so far
+							/* Case 2: we've found a valid snippet
+							 * check its length versus the best so far
+							 */
 							$last_position = key(array_slice($matches, -1, 1, TRUE));
 							if ($shortest == 0 || 
-								$last_position - $first_position < $shortest) {
-								$shortest = $last_position - $first_position;
+								$last_position - $first_position + 1 < $shortest) {
+								$shortest = $last_position - $first_position + 1; // add one to get the correct word count
 								$shortest_snip = implode($snippet, " ");
 							}
+							// Once the check is done, remove the first matching word
 							unset($matches[$first_position]);
 						} else {
-							// Case 3: not all the search words have been found
-							// a word other than the first word was found twice
-							// continue searching until we find all the search words
+							/* Case 3: not all the search words have been found
+							 * a word other than the first word was found twice
+							 * continue searching until we find all the search words
+							 */
 							break;
 						}
-						// The first word in matches has been removed
-						// need to shift the words in the snippet out until the next match is reached
+						/* The first word in matches has been removed
+						 * need to shift the words in the snippet out until the next match is reached
+						 */
 						$next_match = array_shift(array_values($matches));
 						array_shift($snippet);	//shift out the previous matching search word from the snippet
 						while (count($snippet) > 0 && 
-							trim(array_shift(array_values($snippet)), ",.;:/\\ \n\t\r") != $next_match) {
+							strtolower(trim(array_shift(array_values($snippet)), ",.;:/\\ \n\t\r")) != $next_match) {
 							array_shift($snippet);
 						}
 					}
